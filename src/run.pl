2278ee7952b790878086ebb26a26de321f6ecee3
zcx <zcx@umich.edu>
2024-01-11 12:00:08 -0500

    explain db; loop through query_list

--- src/run.pl
+++ src/run.pl
@@ -30,19 +30,25 @@ my $uniprot_db = "uniref90.fasta";
 
 system qq(src/read_list_psibls in_data/query_list $uniprot_db);
 system qq(src/get_assigned_GO_terms_psibls in_data/query_list);
-system qq(perl src/one_line.pl Query1);
-system qq(perl src/filter_60_get_maxid.pl Query1);
-system qq(src/func_pred_psibls Query1);
-system qq(src/divide_categories_psibls);
-system qq(perl src/adjust_format.pl Query1);
-system qq(src/produce_prob MF Query1);
-system qq(src/produce_prob CC Query1);
-system qq(src/produce_prob BP Query1);
-system qq(src/correct_parent_child_psibls MF Query1);
-system qq(src/correct_parent_child_psibls CC Query1);
-system qq(src/correct_parent_child_psibls BP Query1);
-system qq(perl src/combine.pl Query1);
 
+open(FH,"in_data/query_list");
+while(my $line=<FH>)
+{
+    chomp($line);
+    system qq(perl src/one_line.pl $line);
+    system qq(perl src/filter_60_get_maxid.pl $line);
+    system qq(src/func_pred_psibls $line);
+    system qq(src/divide_categories_psibls);
+    system qq(perl src/adjust_format.pl $line);
+    system qq(src/produce_prob MF $line);
+    system qq(src/produce_prob CC $line);
+    system qq(src/produce_prob BP $line);
+    system qq(src/correct_parent_child_psibls MF $line);
+    system qq(src/correct_parent_child_psibls CC $line);
+    system qq(src/correct_parent_child_psibls BP $line);
+    system qq(perl src/combine.pl $line);
+}
+close(FH);
 exit();
 
 sub mkdir
